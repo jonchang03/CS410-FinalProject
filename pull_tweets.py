75e15059b9da9358b7713e8f07965c1f3934f5b0
David <dkiernic@terpmail.umd.edu>
2019-11-25 23:12:18 -0500

    adding clustering

--- pull_tweets.py
+++ pull_tweets.py
@@ -5,6 +5,35 @@ import json
 import datetime
 import re
 from textblob import TextBlob 
+import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+
+def remove_stopwords(tweet) :
+    stop_words = set(stopwords.words('english')) 
+    word_tokens = word_tokenize(tweet)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+  
+    filtered_sentence = [] 
+  
+    for w in word_tokens: 
+        if w not in stop_words: 
+            filtered_sentence.append(w) 
+  
+    return filtered_sentence
+
+def remove_punctuation(tweet) :
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    no_punct = ""
+    for char in my_str:
+        if char not in punctuations:
+            no_punct = no_punct + char
+    # display the unpunctuated string
+    return no_punct
 
 def get_tweets(handle='JoeBiden') :
     """
@@ -40,14 +69,46 @@ def get_tweets(handle='JoeBiden') :
         with open(path, 'w') as json_file:
             json.dump(final_dict, json_file)
 
+def import_from_json(filename) :
+    with open(filename, 'r') as jsonfile:
+        data = json.load(jsonfile)
+    return data
+
+def json_extract_text(json_data) :
+    all_text = []
+    for key in json_data.keys() : 
+        all_text.append(json_data[key]['text'])
+    return all_text
+
+
+
 def main():
     """
     We will be pulling tweets
     """
     candidate_handles = ['JoeBiden', 'SenWarren', 'BernieSanders', 'PeteButtigieg', 'KamalaHarris']
     # other handles candidate_handles = ['AndrewYang', 'amyklobuchar']
-    for h in candidate_handles:
-        get_tweets(handle=h)
+    # for h in candidate_handles:
+    #     get_tweets(handle=h)
+    data = import_from_json("../SenSanders.json")
+    text = json_extract_text(data)
+
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X = vectorizer.fit_transform(text)
+
+    true_k = 4
+    model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+    model.fit(X)
+
+    print("Top terms per cluster:")
+    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+    for i in range(true_k):
+        print("Cluster %d:" % i),
+        for ind in order_centroids[i, :10]:
+            print(' %s' % terms[ind]),
+        print
+
 
 if __name__ == '__main__':
     main()
